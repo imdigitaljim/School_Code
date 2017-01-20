99c6bfae51ff87291ba5eb84585c563064ff68f0
imdigitaljim <imdigitaljim@gmail.com>
2017-01-20 14:22:33 -0500

    pushing fresh copy

--- Offensive Network Security/Buffer Overflow/server.c
+++ Offensive Network Security/Buffer Overflow/server.c
@@ -8,19 +8,16 @@ Save as server-demo6.c
 #include <netinet/in.h>
 #include <stdio.h>
 #include <netdb.h>
-#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #define PORT "55555"
-int readbuff(char *str)
-{
+int readbuff(char *str){
 	char got[200];
 	strcpy(got,str); printf("MSG = %s\n",got);
 	return 0;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 	struct sockaddr_in6 from;
 	struct addrinfo req, *ans;
 	int code, s, s2, len, retval;
@@ -31,28 +28,26 @@ int main(int argc, char *argv[])
 	req.ai_socktype = SOCK_STREAM;
 	req.ai_protocol = 0;
 	retval = getaddrinfo(NULL,PORT,&req,&ans);
-	if(retval!=0)
-	{
+	if(retval!=0){
 		printf("ERROR!getaddrinfo\n");
 		exit(1);
 	}
 	s = socket(ans->ai_family, ans->ai_socktype, ans->ai_protocol);
-	if(s<0)
-	{
+	if(s<0){
 		printf("ERROR!socket\n");
 		exit(1);
 	}
-	if (bind(s, ans->ai_addr, ans->ai_addrlen) < 0)
-	{
+	if (bind(s, ans->ai_addr, ans->ai_addrlen) < 0){
 		printf("ERROR!bind\n");
 		exit(1);
 	}
 	listen(s,5);
-	while(1)
-	{
+	while(1){
 		s2,len = sizeof(from);
 		s2 = accept(s, (struct sockaddr *) &from, &len);
-		if(s2<0) continue;
+		if(s2<0){
+			continue;
+		}
 		send(s2,"IPv6 Demo Server v0.01\n\r",32,0);
 		recv(s2, buff, sizeof(buff),0);
 		readbuff(buff);
